4e617cab3ba4bfc606e085f6d00c3fb6a76ac785
annrayc <anna.raycay@gmail.com>
2017-05-15 01:26:41 +1000

    Tickets can now be edited

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,6 @@ class Ticket < ApplicationRecord
   belongs_to :project
 
   validates :name, presence: true
-  validates :description, presence: true
   validates :description, length: { maximum: 1000 }
   validates :description, presence: true, length: { minimum: 10 }
 end
